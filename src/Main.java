0ad7733acdf55fa071a5c1684130e4a55fa9998b
Aleksandra <zharovaaleksis@gmail.com>
2022-12-06 17:25:28 +0300

    AAAAll new

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 500;
+        int bonus = 20;
+        int miles = amount / bonus;
+        System.out.println("Бонус равен " + miles + " миля(ь).");
+    }
+}
